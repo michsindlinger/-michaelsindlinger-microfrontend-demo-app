5112be1df087a71bd49fec89841345ba728920ac
Michael Sindlinger <michael@rockstardevelopers.de>
2021-01-20 21:12:36 +0100

    Updates

---
+++ precache-manifest.ea1249429c4ac705d6312d431a4d3879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6bf487f585b440dc1049bd60b37862",
+    "url": "./index.html"
+  },
+  {
+    "revision": "13d2a9e34ec3ff6eaff1",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "d0a8b896c274142310f4",
+    "url": "./static/js/2.ba3c9393.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ba3c9393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d2a9e34ec3ff6eaff1",
+    "url": "./static/js/main.6faac981.chunk.js"
+  },
+  {
+    "revision": "58478bc8aaf0d469ca80",
+    "url": "./static/js/runtime-main.7952c026.js"
+  }
+]);
